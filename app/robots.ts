312dda9b8541c3e52ce7bba64847d08e3c58dab6
Ayan <aqiindia.a2mation@gmail.com>
2026-01-20 12:09:33 +0530

    feat: add native sitemap using Next.js app router

---
+++ app/robots.ts
@@ -0,0 +1,13 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+    return {
+        rules: [
+            {
+                userAgent: "*",
+                allow: "/",
+            },
+        ],
+        sitemap: "https://a2aqi.com/sitemap.xml",
+    };
+}
