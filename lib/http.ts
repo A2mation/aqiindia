5dd8ce493771aa700a4f3532e507de305d409ed1
Ayan <aqiindia.a2mation@gmail.com>
2026-01-29 18:16:48 +0530

    refactor(api): remove request timeout

--- lib/http.ts
+++ lib/http.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const http = axios.create({
   baseURL: process.env.NEXT_PUBLIC_SITE_URL,
-  timeout: 10000,
+  // timeout: 15000,
   validateStatus: () => true,
   headers: {
     "Content-Type": "application/json",
